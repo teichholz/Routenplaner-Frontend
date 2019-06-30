f419033070b8133f6d522dea6f49759391e0a1c2
meichholz <tim.eichholz98@web.de>
2019-06-30 21:43:31 +0200

    google maps

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { RestService } from '../rest.service';
-import { keyShape } from './../interfaces/keyShape'
+import { keyShape } from './../interfaces/shapes'
+import { Observable } from 'rxjs';
+import { GoogleMapsAPIWrapper, MarkerManager } from '@agm/core';
 
 @Component({
   selector: 'app-map',
@@ -9,21 +11,25 @@ import { keyShape } from './../interfaces/keyShape'
 })
 export class MapComponent implements OnInit {
   coords = [
-      { lat: 19, lng: 19 },
-      { lat: 20, lng: 20 }
+    { lat: 19, lng: 19 },
+    { lat: 20, lng: 20 }
   ];
   markers = [
-      { lat: 20, lng: 20 },
-      { lat: 21, lng: 21 },
-      { lat: 22, lng: 22 },
-      { lat: 23, lng: 23 }
+    { lat: 20, lng: 20 },
+    { lat: 21, lng: 21 },
+    { lat: 22, lng: 22 },
+    { lat: 23, lng: 23 }
   ];
+  pois = []
+  selectedPoi = []
+  location;
   marker;
-  url=null;
-  key=null;
+  maptype = 'satelite'
+  url = null;
+  key = null;
   coord = { lat: 20, lng: 20 };
 
-  constructor(private http : RestService) { }
+  constructor(private http: RestService) { }
 
   // loadExternalScript(url){
   //   return new Promise(resolve => {
@@ -36,14 +42,34 @@ export class MapComponent implements OnInit {
 
 
   ngOnInit() {
-    this.http.getKey().subscribe((key : keyShape) => 
-    {
+    this.http.getKey().subscribe((key: keyShape) => {
       this.key = key.key
       this.url = "https://maps.googleapis.com/maps/api/js?key=" + this.key + "&callback=initMap";
       console.log(this.url);
       // this.loadExternalScript(this.url);
-      
+      this.getLocation();
+      // this.getPois();
     });
+  }
+
+  setMapType(type) {
+
+  }
+
+  getPois() {
+    this.http.getPois(this.location, 50).subscribe((pois : any[]) => {
+      console.log(pois);
+      this.pois = pois;
+    })
+  }
+
+  getLocation() {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition(loc => {
+        this.location = loc.coords;
+        this.getPois();
+      });
+    }
   }
 
 }
--- src/app/rest.service.ts
+++ src/app/rest.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { keyShape } from './interfaces/keyShape'
+import { keyShape } from './interfaces/shapes'
 
 @Injectable({
   providedIn: 'root'
@@ -16,6 +16,16 @@ export class RestService {
 
   getKey(){
     const res = this.http.get<keyShape>("http://localhost:8080/key/google"); 
+    return res;
+  }
+
+  getPois(location, radius){
+    console.log(location);
+    
+    const url = "http://localhost:8080/poi/get?latitude=" + location.latitude +
+                "&longitude=" + location.longitude +
+                "&radius=" + radius;
+    const res = this.http.get(url); 
     return res;
   }
 }
